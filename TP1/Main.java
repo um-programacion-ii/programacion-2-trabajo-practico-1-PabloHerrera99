b2a7a275daa5c0a1aaeea5fd3aa02e290d06070f
PabloHerrera99 <p.herrera@alumno.um.edu.ar>
2025-04-07 16:54:24 -0300

    Added ArrayLIst to manage a list of vehicles

--- TP1/Main.java
+++ TP1/Main.java
@@ -1,41 +1,43 @@
 package TP1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Vehicle vehicle1 = new Vehicle("AB123CD",
+        List<Vehicle> vehicles = new ArrayList<>();
+
+        vehicles.add(new Vehicle("AB123CD",
                 "Toyota",
                 2025,
-                1200.0);
-        Vehicle vehicle2 = new Vehicle("XYZ789",
+                1200.0));
+        vehicles.add(new Vehicle("XYZ789",
                 "Ford",
                 2019,
-                1500.0);
-        Vehicle vehicle3 = new Vehicle("MNP123",
+                1500.0));
+        vehicles.add(new Vehicle("MNP123",
                 "Chevrolet",
                 2010,
-                1000.0);
-        Truck truck1 = new Truck("OPQ345",
+                1000.0));
+        vehicles.add(new Truck("OPQ345",
                 "Volvo",
                 2015,
                 12000.0,
-                true);
-        Truck truck2 = new Truck("RS678TO",
+                true));
+        vehicles.add(new Truck("RS678TO",
                 "Volvo",
                 2024,
                 15000,
-                false);
-        Car car1 = new Car("GH678IJ",
+                false));
+        vehicles.add(new Car("GH678IJ",
                 "Honda",
                 2020,
                 500,
-                2);
+                2));
 
         VehiclePrinter printer = new VehiclePrinter();
-        printer.print(vehicle1);
-        printer.print(vehicle2);
-        printer.print(vehicle3);
-        printer.print(truck1);
-        printer.print(truck2);
-        printer.print(car1);
+        for (Vehicle vehicle : vehicles) {
+            printer.print(vehicle);
+        }
     }
 }
